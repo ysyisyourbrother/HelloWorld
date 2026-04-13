015a11cdcf7822f14069585ec2f74135994d6a4e
UnsleepingDawn <1205611978@qq.com>
2026-04-13 17:38:31 +0800

    chore

--- bench.sh
+++ bench.sh
@@ -5,6 +5,7 @@
 #     --config configs/symconfig.json > symphony_v1_short_resume.out 2>&1 & echo $! > symphony_v1_short_resume.pid
 
 nohup python launch_benchmark_symphony_v3.py \
+    --subset short \
     --config configs/symconfig_v3.json > symphony_v3_short.out 2>&1 & echo $! > symphony_v3_short.pid
 
 # 断点续跑 v3 时取消下行注释并填写已有 JSON 路径：
