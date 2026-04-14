5b1fab9e2ffa7e43f4af69a8b5694c4b75bfe2cd
UnsleepingDawn <1205611978@qq.com>
2026-04-14 16:10:55 +0800

    chore

--- save_single_frame.py
+++ save_single_frame.py
@@ -3,7 +3,10 @@
 from src.video_utils.about_frame import extract_save_frame_by_index, extract_save_frame_by_time
 
 if __name__ == "__main__":
-    video_path = "local_datasets/Video-MME/data/44ivpEIcBhE.mp4"
-    output_path = "motivation_results/frames/frame.jpg"
-    extract_save_frame_by_time(video_path, output_path, 24.0)
+    video_path = "demo/assets/44ivpEIcBhE.mp4"
+    frame_id = 485
+    # output_path = f"frame_{frame_id}.png"
+    output_path = f"frame.png"
+    # extract_save_frame_by_time(video_path, output_path, 24.0)
+    extract_save_frame_by_index(video_path, output_path, frame_id)
     print(f"已保存到 {output_path}")
